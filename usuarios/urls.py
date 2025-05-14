3c11ba3ac4f72253ccc83a7910adfbbce8fb6696
Landuurra <camilanducci7@gmail.com>
2025-05-14 19:42:17 -0300

    Reestablecer contraseña y boton que redirige a registrar desde el login

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,10 +1,15 @@
 
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import registrarse, login, cerrar_sesion
+from .views import registrarse, login, cerrar_sesion, PswrdResetView, PswrdResetDoneView, PswrdResetConfirmView, PswrdResetCompleteView
 urlpatterns = [
     path('registrarse/', registrarse, name='registrarse'),
     path('login/', login, name='login'),
-     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+     # Rutas para recuperación de contraseña
+    path('password-reset/', PswrdResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', PswrdResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PswrdResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', PswrdResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import redirect, render
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login as auth_login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm, SetPasswordForm
 from django.contrib import messages
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.urls import reverse_lazy
 
 def registrarse(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-             messages.success(request, "Usuario registrado exitosamente")
+            messages.success(request, "Usuario registrado exitosamente")
             return redirect('login')  
     else:
         form = CustomUserCreationForm()
@@ -38,4 +40,25 @@ def login(request):
 def cerrar_sesion(request):
     logout(request)
     messages.success(request, "Se cerró la sesión correctamente")  
-    return redirect('/') 
+    return redirect('/')
+
+class PswrdResetView(PasswordResetView):
+    template_name = 'usuarios/password_reset_form.html'
+    email_template_name = 'usuarios/password_reset_email.html'
+    subject_template_name = 'usuarios/password_reset_subject.txt'
+    success_url = reverse_lazy('password_reset_done')
+    form_class = PasswordResetForm
+
+class PswrdResetDoneView(PasswordResetDoneView):
+    template_name = 'usuarios/password_reset_done.html'
+
+class PswrdResetConfirmView(PasswordResetConfirmView):
+    template_name = 'usuarios/password_reset_confirm.html'
+    success_url = reverse_lazy('password_reset_complete')
+    form_class = SetPasswordForm
+
+class PswrdResetCompleteView(PasswordResetCompleteView):
+    template_name = 'usuarios/password_reset_complete.html'
+    
+    
+    
