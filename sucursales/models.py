a263cad248201bbf2c1dffc388a80c7c62cd00d1
koko <koko_pincha@hotmail.com>
2025-05-16 17:40:33 -0300

    Agrego info de las sucursales al inicio

--- sucursales/models.py
+++ sucursales/models.py
@@ -6,6 +6,7 @@ from datetime import date
 class Sucursal(models.Model):
     nombre = models.CharField(max_length=50)  
     direccion = models.CharField(max_length=255, blank=True, null=True)
+    telefono = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.nombre 
