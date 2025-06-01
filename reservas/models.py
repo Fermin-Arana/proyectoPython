5ea64e6bc79255a2f9746da70d5a2f8f4da5c1e0
koko <koko_pincha@hotmail.com>
2025-06-01 16:24:31 -0300

    Arreglo error en historial

--- reservas/models.py
+++ reservas/models.py
@@ -16,7 +16,7 @@ class Reserva(models.Model):
 
     def precio_total(self):
         if self.fecha_inicio and self.fecha_fin and self.vehiculo:
-            dias = (self.fecha_fin - self.fecha_inicio).days + 1
+            dias = (self.fecha_fin - self.fecha_inicio).days
             return dias * self.vehiculo.precio_por_dia
         return 0
 
